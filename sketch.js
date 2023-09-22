b5f4e6f945b5ef496bf9815687fb07f0adf1f830
3 <en.mail.til.oscar@gmail.com>
2023-09-22 11:45:28 +0200

    første omgang copypaste med lidt random

--- sketch.js
+++ sketch.js
@@ -1,7 +1,23 @@
+let bubbler = []
+let Antal = 10
+
 function setup() {
   createCanvas(400, 400);
+  //laver variabel mængde bubbler
+  for(i++;i<Antal;i++){
+    append(bubbler,new Bubbles(
+      random(0,width),
+      random(0,height),
+      random(0,40)))
+  }
 }
 
 function draw() {
   background(220);
+  for(i=0;i<bubbler.length;i++){
+    bubbler[i].show()
+    bubbler[i].move()
+
+  }
+
 }
