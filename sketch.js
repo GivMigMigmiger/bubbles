49e161d724aa88e2d3fbcb459612ff4c01209c51
3 <en.mail.til.oscar@gmail.com>
2023-09-22 11:54:27 +0200

    I princippet færdig
    kan nu fjerne og lave nye bubbler

--- sketch.js
+++ sketch.js
@@ -1,5 +1,6 @@
 let bubbler = []
 let Antal = 10
+let BubRadMax = 40
 
 function setup() {
   createCanvas(400, 400);
@@ -8,16 +9,29 @@ function setup() {
     append(bubbler,new Bubbles(
       random(0,width),
       random(0,height),
-      random(0,40)))
+      random(0,BubRadMax)))
   }
 }
 
 function draw() {
   background(220);
   for(i=0;i<bubbler.length;i++){
+    //bevæger og viser alle bublerne
     bubbler[i].show()
     bubbler[i].move()
-
+    //tjekker om bublerne er over skærmen
+    if(bubbler[i].y+bubbler[i].r<0){
+      //splicer elementet
+      let Temp1 = asteroider.slice(0,minIndex)
+      let Temp2 = asteroider.slice(minIndex+1,asteroider.length)
+      bubbler = concat(astTemp1,astTemp2)
+      //laver en ny bubbel
+      let tempR = random(0,BubRadMax)
+      append(bubbler,new Bubbles(
+        random(0,width),
+        heigt+tempR,
+        random(0,BubRadMax)))
+    }
   }
 
 }
