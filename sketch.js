ea39cb69e2b913cb863b69dc672bcfb6296afd6e
3 <en.mail.til.oscar@gmail.com>
2023-09-22 12:05:04 +0200

    bug fixes + min størrelse
    læste faktisk koden jeg copierede igennem, og tilføjede BubRadMin

--- sketch.js
+++ sketch.js
@@ -1,14 +1,17 @@
 let bubbler = []
 let Antal = 10
 let BubRadMax = 40
+let BubRadMin = 10
 
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(windowWidth, windowHeight);
   //laver variabel mængde bubbler
   for(i++;i<Antal;i++){
-    //tempR gør det muligt at være meget tæt på udenforskærmen
-    let tempR = random(0,BubRadMax)
-    append(bubbler,new Bubbles(
+    //tempR gør det muligt at være meget tæt på udenfor skærmen
+    //BubRadMin og Max gøre bublerne har en god størrelse
+    let tempR = random(BubRadMin,BubRadMax)
+    append(bubbler,
+      new Bubbles(
       random(-tempR,width+tempR),
       random(-tempR,height+tempR),
       tempR))
@@ -24,16 +27,19 @@ function draw() {
     //tjekker om bublerne er over skærmen
     if(bubbler[i].y+bubbler[i].r<0){
       //splicer elementet
-      let Temp1 = asteroider.slice(0,minIndex)
-      let Temp2 = asteroider.slice(minIndex+1,asteroider.length)
+      let Temp1 = bubbler.slice(0,i)
+      let Temp2 = bubbler.slice(i+1,bubbler.length)
       bubbler = concat(astTemp1,astTemp2)
       //laver en ny bubbel
       //bruger radius til at bestemme hvor langt nede den skal være
-      let tempR = random(0,BubRadMax)
-      append(bubbler,new Bubbles(
+      let tempR = random(BubRadMin,BubRadMax)
+      append(bubbler,
+        new Bubbles(
         random(-tempR,width),
         heigt+tempR,
         tempR))
+        //rykker i én tilbage fordi den ellers springer en over
+        i -= 1
     }
   }
 
